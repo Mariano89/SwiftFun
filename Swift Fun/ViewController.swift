03ed6060c07fd363e8ecbe7a9280359a0c1fe32a
Mariano Echegoyen <mariano.echegoyen@dng.com>
2017-12-12 11:05:15 -0800

    add switch button to add / subtract input values

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -3,27 +3,32 @@
 //  Swift Fun
 //
 //  Created by Mariano Echegoyen on 11/1/17.
-//  Copyright © 2017 Mariano Echegoyen. All rights reserved.
+//  Copyright © 2017 Mariano Echegoyen. All rights reserved..
 //
 
 import UIKit
 
 class ViewController: UIViewController {
     
-    var buttonCount = 0
     @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    @IBOutlet weak var additionSwitch: UISwitch!
     
     @IBAction func buttonTapped(_ sender: Any) {
         
-        buttonCount += 1
+        let addition = additionSwitch.isOn
         
-        print(buttonCount)
-        
-        if buttonCount >= 10 {
-            view.backgroundColor = UIColor.red
+        if addition {
+            let sum = Double(topTextField.text!)! + Double(bottomTextField.text!)!
             
-            myLabel.text = "You hit it more than 10 times!"
+            myLabel.text = "\(topTextField.text!) + \(bottomTextField.text!) = \(sum)"
+        } else {
+            let sum = Double(topTextField.text!)! - Double(bottomTextField.text!)!
+            
+            myLabel.text = "\(topTextField.text!) - \(bottomTextField.text!) = \(sum)"
         }
+        
         
     }
     
